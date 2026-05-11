0ccab73ad8c1b82cc83a89a949ee33435c729a66
Changsheng Quan <quancs@qq.com>
2026-05-11 13:36:28 +0800

    Handle pow function import with fallback
    Signed-off-by: Changsheng Quan <quancs@qq.com>

--- vllm_ascend/worker/v2/sample/penalties.py
+++ vllm_ascend/worker/v2/sample/penalties.py
@@ -21,7 +21,10 @@
 import torch
 from vllm.triton_utils import tl, triton
 
-pow = triton.language.extra.ascend.libdevice.pow
+try:
+    pow = triton.language.extra.ascend.libdevice.pow
+except:
+    pow = triton.language.extra.cann.libdevice.pow
 
 
 @triton.jit
